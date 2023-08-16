9501711d15f751bef38c53a53c89a1a5e21473be
yong1le <ygle.he@gmail.com>
2023-08-16 11:53:13 -0400

    add prettier and eslint

--- app/questions/[id]/page.js
+++ app/questions/[id]/page.js
@@ -1,35 +1,37 @@
-'use client'
+"use client";
 import QuestionCard from "@/app/components/QuestionCard";
 import QuestionQuery from "@/app/components/QuestionQuery";
-import { checkId, getSpecificQuestion } from "@/app/utils/Model";
+import { getSpecificQuestion } from "@/app/utils/Model";
 import { navigateFromMemory } from "@/app/utils/Navigation";
-import { ExpandLess, ExpandMore, Navigation } from "@mui/icons-material";
-import { Breadcrumbs, Button, Collapse, Container, IconButton, Typography } from "@mui/material";
-import ErrorPage from 'next/error';
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import {
+  Breadcrumbs,
+  Button,
+  Collapse,
+  Container,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import ErrorPage from "next/error";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { data } from "../../utils/db";
-
 
 export default function page({ params }) {
-
   // router stuff
   const router = useRouter();
   const nextQuestion = () => {
     navigateFromMemory(router);
-  }
+  };
 
   const question = getSpecificQuestion(params.id);
   if (question == null) {
-    return (
-      <ErrorPage statusCode={404} />
-    )
+    return <ErrorPage statusCode={404} />;
   }
 
   // Opening/Closing the "search"
   const expandQuery = () => {
-    setQueryExpanded(!queryExpanded)
-  }
+    setQueryExpanded(!queryExpanded);
+  };
   const [queryExpanded, setQueryExpanded] = useState(false);
 
   return (
@@ -43,21 +45,21 @@ export default function page({ params }) {
         sx={{
           display: "flex",
           alignItems: "center",
-          justifyContent: "center"
-        }}>
-        <IconButton
-          color="primary"
-          onClick={expandQuery}
-        >
+          justifyContent: "center",
+        }}
+      >
+        <IconButton color="primary" onClick={expandQuery}>
           {queryExpanded ? <ExpandLess /> : <ExpandMore />}
         </IconButton>
       </Container>
 
       {/* Question */}
-      <Container fixed
+      <Container
+        fixed
         sx={{
-          paddingTop: 10
-        }}>
+          paddingTop: 10,
+        }}
+      >
         <Breadcrumbs>
           <Typography>Unit {question.unit}</Typography>
           <Typography>Chapter {question.chapter}</Typography>
@@ -71,23 +73,20 @@ export default function page({ params }) {
         />
 
         {/* Nav */}
-        <Container fixed disableGutters
+        <Container
+          fixed
+          disableGutters
           sx={{
             display: "flex",
             justifyContent: "flex-end",
             marginY: 10,
-          }}>
-          <Button
-            onClick={nextQuestion}
-            variant="contained"
-          >
+          }}
+        >
+          <Button onClick={nextQuestion} variant="contained">
             Next Question
           </Button>
         </Container>
-
       </Container>
-
     </Container>
-  )
-
+  );
 }
